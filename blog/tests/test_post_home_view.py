4ab02bc457c785a2000d25ae28f5ce0052b50e79
Jonathas Emanuel <jonathas92244@gmail.com>
2024-05-24 19:20:38 -0300

    more tests for home view

--- blog/tests/test_post_home_view.py
+++ blog/tests/test_post_home_view.py
@@ -1,6 +1,7 @@
 from django.urls import resolve, reverse
 from blog import views
 from test_post_base import PostTestBase
+from unittest.mock import patch
 
 
 class PostHomeViewTest(PostTestBase):
@@ -26,3 +27,26 @@ class PostHomeViewTest(PostTestBase):
         reponse = self.client.get(reverse('blog:home'))
         content = reponse.content.decode('utf-8')
         self.assertIn('Test', content)
+
+    def test_blog_home_template_dont_load_not_published(self):
+        self.make_post(is_published=False)
+        response = self.client.get(reverse('blog:home'))
+        self.assertIn(
+            'No posts found',
+            response.content.decode('utf-8')
+        )
+
+    @patch('blog.views.listview_post.PER_PAGE', new=11)
+    def test_blog_home_is_paginated(self):
+        wanted_number_posts = 11
+        self.make_post_in_bath(wanted_number_posts)
+        response = self.client.get(reverse('blog:home'))
+        qtd_of_posts_in_page = len(response.context.get('posts'))
+
+        self.assertEqual(qtd_of_posts_in_page, wanted_number_posts)
+
+    @patch('blog.views.listview_post.PER_PAGE', new=11)
+    def test_invalid_page_query_uses_first_page(self):
+        self.make_post_in_bath(11)
+        response = self.client.get(reverse('blog:home'), {'page': 2})
+        self.assertEqual(response.context['posts'].number, 1)

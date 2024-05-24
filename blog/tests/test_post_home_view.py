ee22033c515c1c45095f3bb29d069431431ee9ce
Jonathas Emanuel <jonathas92244@gmail.com>
2024-05-24 07:55:38 -0300

    more test for home view

--- blog/tests/test_post_home_view.py
+++ blog/tests/test_post_home_view.py
@@ -1,11 +1,28 @@
 from django.urls import resolve, reverse
 from blog import views
-from .test_post_base import PostTestBase
-from unittest.mock import patch
+from test_post_base import PostTestBase
 
 
 class PostHomeViewTest(PostTestBase):
 
-    def test_post_home_view_function_is_correct(self):
+    def test_blog_home_view_function_is_correct(self):
         view = resolve(reverse('blog:home'))
-        self.assertIn(view.func.view_class, views.PostListViewHome)
+        self.assertIs(view.func.view_class, views.PostListViewHome)
+
+    def test_blog_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('blog:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_home_template_load_is_correct(self):
+        response = self.client.get(reverse('blog:home'))
+        self.assertTemplateUsed(response, 'blog/page/home.html')
+
+    def test_blog_home_template_shows_no_posts_found_if_no_recipes(self):
+        response = self.client.get(reverse('blog:home'))
+        self.assertContains(response, 'No posts found')
+
+    def test_blog_home_template_load_posts(self):
+        self.make_post()
+        reponse = self.client.get(reverse('blog:home'))
+        content = reponse.content.decode('utf-8')
+        self.assertIn('Test', content)

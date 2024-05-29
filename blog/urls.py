3e4e51c9283deeef63f489eb412ccfca04b42f2c
Jonathas Emanuel <jonathas92244@gmail.com>
2024-05-29 19:51:52 -0300

    add: view for search

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostListViewHome.as_view(), name='home'),
     path('post/<int:pk>/', views.DetailPost.as_view(), name='detail_post'),
+    path('search/', views.PostListViewSearch.as_view(), name='search'),
 ]

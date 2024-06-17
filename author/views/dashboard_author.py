ed30ba856d801c8c33dbcdf64fd06b7ef7cd3f42
Jonathas Emanuel <jonathas92244@gmail.com>
2024-06-17 12:18:04 -0300

    added the function to edit and view unpublished posts for the blog administrator

--- author/views/dashboard_author.py
+++ author/views/dashboard_author.py
@@ -39,3 +39,37 @@ class DashboardAuthorIsPublished(ListView):
         )
 
         return context
+
+
+@method_decorator(login_required(
+    login_url='blog:home', redirect_field_name='next'), name='dispatch')
+class DashboardAuthorUnpublished(ListView):
+    model = Post
+    template_name = 'author/page/dashboard_author_post_unpublished.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(
+            author=self.request.user,
+            is_published=False)
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request,
+            context.get('posts'),
+            PER_PAGE,
+        )
+        context.update(
+            {
+                'profile': self.request.user.username,
+                'posts': page_obj,
+                'page_obj': pagination_range,
+            }
+
+        )
+
+        return context
